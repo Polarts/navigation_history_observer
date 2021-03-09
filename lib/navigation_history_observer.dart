d62cf6259dea522ea8a24d29a09524de22ce0989
Emanuele Salamone <emanuele.salamone@a-tono.com>
2021-03-09 12:49:31 +0100

    Migrated to null-safety

--- lib/navigation_history_observer.dart
+++ lib/navigation_history_observer.dart
@@ -8,24 +8,24 @@ import 'package:flutter/widgets.dart';
 class NavigationHistoryObserver extends NavigatorObserver {
 
   /// A list of all the past routes
-  final List<Route<dynamic>> _history = <Route<dynamic>>[];
+  final List<Route<dynamic>?> _history = <Route<dynamic>?>[];
 
   /// Gets a clone of the navigation history as an immutable list.
   BuiltList<Route<dynamic>> get history =>
       BuiltList<Route<dynamic>>.from(_history);
 
   /// Gets the top route in the navigation stack.
-  Route<dynamic> get top => _history.last;
+  Route<dynamic>? get top => _history.last;
 
   /// A list of all routes that were popped to reach the current.
-  final List<Route<dynamic>> _poppedRoutes = <Route<dynamic>>[];
+  final List<Route<dynamic>?> _poppedRoutes = <Route<dynamic>?>[];
 
   /// Gets a clone of the popped routes as an immutable list.
   BuiltList<Route<dynamic>> get poppedRoutes =>
       BuiltList<Route<dynamic>>.from(_poppedRoutes);
 
   /// Gets the next route in the navigation history, which is the most recently popped route.
-  Route<dynamic> get next => _poppedRoutes.last;
+  Route<dynamic>? get next => _poppedRoutes.last;
 
   /// A stream that broadcasts whenever the navigation history changes.
   final StreamController _historyChangeStreamController =
@@ -42,7 +42,7 @@ class NavigationHistoryObserver extends NavigatorObserver {
   }
 
   @override
-  void didPop(Route<dynamic> route, Route<dynamic> previousRoute) {
+  void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
     _poppedRoutes.add(_history.last);
     _history.removeLast();
     _historyChangeStreamController.add(HistoryChange(
@@ -53,7 +53,7 @@ class NavigationHistoryObserver extends NavigatorObserver {
   }
 
   @override
-  void didPush(Route<dynamic> route, Route<dynamic> previousRoute) {
+  void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
     _history.add(route);
     _poppedRoutes.remove(route);
     _historyChangeStreamController.add(HistoryChange(
@@ -64,7 +64,7 @@ class NavigationHistoryObserver extends NavigatorObserver {
   }
 
   @override
-  void didRemove(Route<dynamic> route, Route<dynamic> previousRoute) {
+  void didRemove(Route<dynamic> route, Route<dynamic>? previousRoute) {
     _history.remove(route);
     _historyChangeStreamController.add(HistoryChange(
       action: NavigationStackAction.remove,
@@ -74,7 +74,7 @@ class NavigationHistoryObserver extends NavigatorObserver {
   }
 
   @override
-  void didReplace({Route<dynamic> newRoute, Route<dynamic> oldRoute}) {
+  void didReplace({Route<dynamic>? newRoute, Route<dynamic>? oldRoute}) {
     int oldRouteIndex = _history.indexOf(oldRoute);
     _history.replaceRange(oldRouteIndex, oldRouteIndex + 1, [newRoute]);
     _historyChangeStreamController.add(HistoryChange(
@@ -89,9 +89,9 @@ class NavigationHistoryObserver extends NavigatorObserver {
 class HistoryChange {
   HistoryChange({this.action, this.newRoute, this.oldRoute});
 
-  final NavigationStackAction action;
-  final Route<dynamic> newRoute;
-  final Route<dynamic> oldRoute;
+  final NavigationStackAction? action;
+  final Route<dynamic>? newRoute;
+  final Route<dynamic>? oldRoute;
 }
 
 enum NavigationStackAction { push, pop, remove, replace }
